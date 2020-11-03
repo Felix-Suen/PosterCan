e9a2fb6486a48c8c6f03522f0a5d823460ebaec4
Felix-Suen <felix.suen2000@gmail.com>
2020-11-03 12:13:09 -0800

    add and remove comment on posters

--- routes/api/posters.js
+++ routes/api/posters.js
@@ -38,7 +38,7 @@ router.post(
 
                 res.json(poster);
             } else {
-                return res.status(401).json({msg: 'User Unauthorized'});
+                return res.status(401).json({ msg: 'User Unauthorized' });
             }
         } catch (err) {
             console.error(err.message);
@@ -66,16 +66,16 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const poster = await Poster.findById(req.params.id);
-        
+
         if (!poster) {
-            return res.status(404).json({ msg: "Poster not found" });
+            return res.status(404).json({ msg: 'Poster not found' });
         }
-        
+
         res.json(poster);
     } catch (err) {
         console.error(err.message);
         if (err.kind === 'ObjectId') {
-            return res.status(404).json({ msg: "Poster not found" });
+            return res.status(404).json({ msg: 'Poster not found' });
         }
         res.status(500).send('server error');
     }
@@ -87,22 +87,22 @@ router.get('/:id', async (req, res) => {
 router.delete('/:id', auth, async (req, res) => {
     try {
         const poster = await Poster.findById(req.params.id);
-        
+
         if (!poster) {
-            return res.status(404).json({ msg: "Poster not found" });
+            return res.status(404).json({ msg: 'Poster not found' });
         }
 
         if (poster.user.toString() !== req.user.id) {
-            return res.status(401).json({ msg: "User not authorized" });
+            return res.status(401).json({ msg: 'User not authorized' });
         }
 
         await poster.remove();
-        
-        res.json({ msg: "Poster removed" });
+
+        res.json({ msg: 'Poster removed' });
     } catch (err) {
         console.error(err.message);
         if (err.kind === 'ObjectId') {
-            return res.status(404).json({ msg: "Poster not found" });
+            return res.status(404).json({ msg: 'Poster not found' });
         }
         res.status(500).send('server error');
     }
@@ -116,7 +116,10 @@ router.put('/like/:id', auth, async (req, res) => {
         const poster = await Poster.findById(req.params.id);
 
         // Check if the poster has already been liked
-        if (poster.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        if (
+            poster.likes.filter((like) => like.user.toString() === req.user.id)
+                .length > 0
+        ) {
             return res.status(400).json({ msg: 'Poster Already Liked' });
         }
 
@@ -139,12 +142,19 @@ router.put('/unlike/:id', auth, async (req, res) => {
         const poster = await Poster.findById(req.params.id);
 
         // Check if the poster has already been liked
-        if (poster.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
-            return res.status(400).json({ msg: 'Poster has not yet been liked' });
+        if (
+            poster.likes.filter((like) => like.user.toString() === req.user.id)
+                .length === 0
+        ) {
+            return res
+                .status(400)
+                .json({ msg: 'Poster has not yet been liked' });
         }
 
         // Get remove index
-        const removeIndex = poster.likes.map(like => like.user.toString()).indexOf(req.user.id);
+        const removeIndex = poster.likes
+            .map((like) => like.user.toString())
+            .indexOf(req.user.id);
 
         poster.likes.splice(removeIndex, 1);
 
@@ -154,6 +164,76 @@ router.put('/unlike/:id', auth, async (req, res) => {
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
+    }
+});
+
+// @route   POST api/posters/comment/:id
+// @desc    Comment on a poster
+// @access  Private
+router.post(
+    '/comment/:id',
+    [auth, [check('text', 'Text is required').notEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const poster = await Poster.findById(req.params.id);
+
+            const newComment = {
+                text: req.body.text,
+                user: req.user.id,
+                name: user.name,
+                avatar: user.avatar,
+            };
+
+            poster.comments.unshift(newComment);
+
+            await poster.save();
+
+            res.json(poster.comments);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('server error');
+        }
+    }
+);
+
+// @route   DELETE api/posters/comment/:id/:comment_id
+// @desc    delete a Comment on a poster
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const poster = await Poster.findById(req.params.id);
+
+        // pull out comment
+        const comment = poster.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure comment exists
+        if (!comment) {
+            return res.status(404).json({ msg: 'comment does not exists' });
+        }
+
+        // User verify
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "user not authorized" });
+        }
+
+        const removeIndex = poster.comments
+            .map((comment) => comment.user.toString())
+            .indexOf(req.user.id);
+
+        poster.comments.splice(removeIndex, 1);
+
+        await poster.save();
+
+        res.json(poster.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
     }
 });
 
