0c229eebea50a801bff0812171131fb9e79bc613
Felix-Suen <felix.suen2000@gmail.com>
2020-11-02 19:17:48 -0800

    user register

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
         });
 
         console.log('mongoDB connected...');
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register User
@@ -12,11 +16,53 @@ router.post(
         check('email', 'Please include a valid email').isEmail(),
         check('password', 'Please Enter a valid password').isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
+
+        const { name, email, password, admin } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            // See if user exists
+            if (user) {
+                return res.status(400).json({
+                    errors: [{ msg: 'User already exists' }],
+                });
+            }
+
+            // Get Users Gravatar
+            const avatar = gravatar.url(email,{
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password,
+                admin
+            });
+
+            // Encrypt Password
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            //Return jsonwebtoken
+            res.send('user registered');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
         res.send('User route');
     }
 );
