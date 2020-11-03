6e59803a8b133e2eadbd8737f90c4e3ba545de59
Felix-Suen <felix.suen2000@gmail.com>
2020-11-03 10:34:19 -0800

    Poster model

---
+++ models/Poster.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PosterSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    images: [
+        {
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    description: {
+        type: String,
+        required: true
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String,
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Poster = mongoose.model('poster', PosterSchema);
