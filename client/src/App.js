2dc082ab6b8cf8e9baebcb91b0a9038499db7396
Felix-Suen <felix.suen2000@gmail.com>
2020-11-04 16:38:08 -0800

    init reducer

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,34 @@ import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
     return (
-        <Router>
-            <div style={{ backgroundImage: `url(https://wallpapercave.com/wp/wp5121842.jpg)` }}>
-                <Navibar />
-                <Route exact path='/' component={Landing} />
-                <section>
-                    <Switch>
-                        <Route exact path="/register" component={Register} />
-                        <Route exact path="/login" component={Login} />
-                    </Switch>
-                </section>
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div
+                    style={{
+                        backgroundImage: `url(https://wallpapercave.com/wp/wp5121842.jpg)`,
+                    }}
+                >
+                    <Navibar />
+                    <Route exact path="/" component={Landing} />
+                    <section>
+                        <Switch>
+                            <Route
+                                exact
+                                path="/register"
+                                component={Register}
+                            />
+                            <Route exact path="/login" component={Login} />
+                        </Switch>
+                    </section>
+                </div>
+            </Router>
+        </Provider>
     );
 };
 
