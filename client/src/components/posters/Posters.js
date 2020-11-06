749d8db0a9007b3585e257697658cc484bca1302
Felix-Suen <felix.suen2000@gmail.com>
2020-11-06 11:36:33 -0800

    display all posts

--- client/src/components/posters/Posters.js
+++ client/src/components/posters/Posters.js
@@ -4,20 +4,43 @@ import { connect } from 'react-redux';
 import { getPosters } from '../../actions/poster';
 import Spinner from '../layouts/Spinner';
 import '../layouts/layouts.css';
+import { Card, Button, CardDeck, Container, Row, Col } from 'react-bootstrap';
 
 const Posters = ({ getPosters, poster: { posters, loading } }) => {
     useEffect(() => {
         getPosters();
     }, [getPosters]);
 
-    return (
-        loading ? <Spinner /> : (
-            <div className="posters">
-                <div>
-                    <h1>Hello</h1>
-                </div>
+    return loading ? (
+        <Spinner />
+    ) : (
+        <div className="posters">
+            <div className="card-container">
+                <Container>
+                    <Row>
+                        {posters &&
+                            posters.map((poster) => (
+                                <Col sm="4">
+                                <Card key={poster.id} style={{ marginBottom: '30px' }}>
+                                    <Card.Img
+                                        variant="top"
+                                        src={poster.images[0]}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>{poster.title}</Card.Title>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <small className="text-muted">
+                                            {poster.date}
+                                        </small>
+                                    </Card.Footer>
+                                </Card>
+                                </Col>
+                            ))}
+                    </Row>
+                </Container>
             </div>
-        )
+        </div>
     );
 };
 
