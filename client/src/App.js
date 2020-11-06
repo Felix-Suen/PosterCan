17634085dbbbb9b494d590bcbd1df9ab08b8fd07
Felix-Suen <felix.suen2000@gmail.com>
2020-11-05 18:32:19 -0800

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navibar from './components/layouts/Navibar';
 import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
+if(localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
 const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, [loadUser]);
+
     return (
         <Provider store={store}>
             <Router>
